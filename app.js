6a697a2a700a1fdeb0fe87695acfc53d37ce79c3
Cheni Chen <celiachenic@gmail.com>
2021-05-29 18:25:19 +0800

    開啟網頁時判斷cookie是否存在，如果存在則自動填入

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let submit = document.querySelector("#submit");
+let id = document.querySelector("#id");
+let password = document.querySelector("#password");
+let remember = document.querySelector("#remember");
 
 //set cookies
 function setCookie(name, value, day) {
@@ -24,19 +27,18 @@ function removeCookie(name) {
   setCookie(name, 1, -1); //調用setCookie function，並將日期減ㄧ
 }
 
-submit.addEventListener("click", function (e) {
-  // e.preventDefault();
+//判斷cookie是否存在，如果存在則自動填入
+if (getCookie("id")) {
+  id.value = getCookie("id");
+  password.value = getCookie("password");
+}
 
-
-
-
-  let id = document.querySelector("#id").value;
-  let password = document.querySelector("#password").value;
-  let remember = document.querySelector("#remember");
-
+submit.addEventListener("click", function () {
+  //有打勾記住帳號密碼
   if (remember.checked) {
-    //有打勾記住帳號密碼
-    setCookie("id", id, 30); //預設記住30天
-    setCookie("password", password, 30);
+    let userId = id.value;
+    let userPassword = password.value;
+    setCookie("id", userId, 30); //預設記住30天
+    setCookie("password", userPassword, 30);
   }
 });
